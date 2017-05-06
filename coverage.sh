14a005640cc8302735ef6f8e26bea070c83b2546
lancetw aka Hsin-lin Cheng <lancetw@gmail.com>
2017-05-06 15:05:15 +0800

    update coverage script

--- coverage.sh
+++ coverage.sh
@@ -1,22 +1,12 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-echo "mode: atomic" > coverage.out
+set -e
+echo "" > coverage.txt
 
-PACKAGES=`govendor list -no-status +local`
-EXIT_CODE=0
-
-for PKG in $PACKAGES; do
-  echo =-= $PKG
-
-  govendor test -v -coverprofile=profile.out -covermode=atomic $PKG; __EXIT_CODE__=$?
-
-  if [ "$__EXIT_CODE__" -ne "0" ]; then
-    EXIT_CODE=$__EXIT_CODE__
-  fi
-
-  if [ -f profile.out ]; then
-    tail -n +2 profile.out >> coverage.out; rm profile.out
-  fi
+for d in $(go list ./... | grep -v vendor); do
+    go test -race -coverprofile=profile.out -covermode=atomic $d
+    if [ -f profile.out ]; then
+        cat profile.out >> coverage.txt
+        rm profile.out
+    fi
 done
-
-exit $EXIT_CODE
