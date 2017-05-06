68475ac8b850c57f433e07144a40339ad9126694
lancetw aka Hsin-lin Cheng <lancetw@gmail.com>
2017-05-06 14:54:56 +0800

    support multiple packages

---
+++ coverage.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+echo "mode: count" > coverage.out
+
+PACKAGES=`govendor list -no-status +local`
+EXIT_CODE=0
+
+for PKG in $PACKAGES; do
+  echo =-= $PKG
+
+  govendor test -v -coverprofile=profile.out -covermode=count $PKG; __EXIT_CODE__=$?
+
+  if [ "$__EXIT_CODE__" -ne "0" ]; then
+    EXIT_CODE=$__EXIT_CODE__
+  fi
+
+  if [ -f profile.out ]; then
+    tail -n +2 profile.out >> coverage.out; rm profile.out
+  fi
+done
+
+exit $EXIT_CODE
